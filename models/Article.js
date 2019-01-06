6eb84cf95cc73255a0a4025800655e3834d9464b
jlrt37 <joshua.l.ragon@gmail.com>
2019-01-06 09:59:43 -0600

    update ui files

--- models/Article.js
+++ models/Article.js
@@ -1,27 +1,32 @@
 var mongoose = require("mongoose");
 
+// Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
 var ArticleSchema = new Schema({
   headline: {
     type: String,
     unique: true
-  },
+    },
   summary: {
     type: String
+    
   },
   url: {
     type: String
-  },
+    },
+
   isSaved: {
     type: Boolean,
     default: false
   },
+
   note: {
     type: Schema.Types.ObjectId,
     ref: "Note"
   }
 });
+
 var Article = mongoose.model("Article", ArticleSchema);
 
 module.exports = Article;
