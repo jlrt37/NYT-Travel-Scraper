b253717a5129adcd0f61c90bd92ab8b6d759b276
jlrt37 <joshua.l.ragon@gmail.com>
2019-01-06 10:23:15 -0600

    updates

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newsScraper";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI, {
